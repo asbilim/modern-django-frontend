933e4696dea1e22bcf2b974ed0e7ae145823036e
unknown <bilimdepaullilian@gmail.com>
2025-06-16 22:27:05 +0100

    style problem fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+export default {
+  plugins: {
+    "postcss-nested": {},
+    "postcss-custom-properties": {
+      preserve: false,
+    },
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
